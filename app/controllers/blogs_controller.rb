e4df4125b731fc2d83a9dbcbf4452de14241f70f
Toshi-Yano <62419040+Toshi-Yano@users.noreply.github.com>
2020-04-14 19:11:06 +0900

    add user-page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,24 @@
 class BlogsController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
     @blogs = Blog.includes(:user, :category)
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    Blog.create(blogs_params)
+  end
+
+  private
+  def blogs_params
+    params.require(:blog).permit(:title, :url, :body)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
