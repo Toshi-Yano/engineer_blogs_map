f01c3469a6d6d1cd899991fc17a461fa16540e23
Toshi-Yano <62419040+Toshi-Yano@users.noreply.github.com>
2020-05-23 23:54:30 +0900

    set factory_bot

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    name                  {"yano"}
+    introduction          {"ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。ちょうど２００文字。"}
+    email                 {"yano@gmail.com"}
+    password              {"aaaa1111"}
+    password_confirmation {"aaaa1111"}
+  end
+end
