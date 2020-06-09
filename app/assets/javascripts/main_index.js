fa9614ec1d34f9c35a03dd46226cad45a1a1668e
Toshi-Yano <62419040+Toshi-Yano@users.noreply.github.com>
2020-06-09 19:43:05 +0900

    middle in the save

--- app/assets/javascripts/main_index.js
+++ app/assets/javascripts/main_index.js
@@ -1,7 +1,9 @@
 $(function() {
   $(document).on('turbolinks:load', ()=> {
 
-
+    const pageCount = 2 // トップページのページ数
+    
+    $(document).on("click", "right-arrow", function() {
 
     });
   });
