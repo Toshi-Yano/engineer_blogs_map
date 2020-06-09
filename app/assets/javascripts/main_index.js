47370d35fb98ee9c9683116088861b10b73459af
Toshi-Yano <62419040+Toshi-Yano@users.noreply.github.com>
2020-06-09 22:38:29 +0900

    add main_index.js

--- app/assets/javascripts/main_index.js
+++ app/assets/javascripts/main_index.js
@@ -1,10 +1,31 @@
 $(function() {
   $(document).on('turbolinks:load', ()=> {
 
-    const pageCount = 2 // トップページのページ数
+    const pageCount = 1 // トップページのコンテンツ数 - 1
     
-    $(document).on("click", "right-arrow", function() {
+    $(document).on("click", "#top-right-arrow", function() {
+      const currentPage = $("#show-index").data("index");
+      const nextPage = currentPage + 1;
 
+      $(".main__index__list").removeAttr("id", "show-index")
+      if (nextPage > pageCount) {
+        $('[data-index = 0]').attr("id", "show-index");
+      } else {
+        $('[data-index="' + nextPage + '"]').attr("id", "show-index");
+      };
     });
+
+    $(document).on("click", "#top-left-arrow", function() {
+      const currentPage = $("#show-index").data("index");
+      const previousPage = currentPage - 1;
+
+      $(".main__index__list").removeAttr("id", "show-index")
+      if (previousPage < 0 ) {
+        $('[data-index="' + pageCount + '"]').attr("id", "show-index");
+      } else {
+        $('[data-index="' + previousPage + '"]').attr("id", "show-index");
+      };
+    });
+    
   });
 })
